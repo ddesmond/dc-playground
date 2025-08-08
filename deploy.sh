9beaea168fccef74dc05b4b880712eec9bf7de16
Aleks <crni.aki@gmail.com>
2025-08-08 17:06:58 +0200

    init

--- deploy.sh
+++ deploy.sh
@@ -15,15 +15,12 @@ docker run -d \
   -e TZ=Etc/UTC \
   -e SUBFOLDER=/  \
   -e TITLE=Webtop  \
-  -e DRINODE=/dev/dri/renderD128 \
   -p 3000:3000 \
   -p 3001:3001 \
   -v /data:/config \
-  -v /var/run/docker.sock:/var/run/docker.sock \
   --shm-size="21gb"  \
   --privileged \
   --gpus all \
   --runtime nvidia \
-  --device /dev/dri:/dev/dri \
   --restart unless-stopped \
   linuxserver/webtop:ubuntu-xfce
