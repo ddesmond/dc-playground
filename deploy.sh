3c9ed379bc4c92b37775a3c029cc67e7257b0b15
Aleks <crni.aki@gmail.com>
2025-08-08 16:55:21 +0200

    init

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
 # deploy playground
+nvidia-ctk runtime configure --runtime=docker --set-as-default
+systemctl restart docker
+mkdir -p /data
 
+docker pull linuxserver/webtop:ubuntu-xfce
+
+docker run -d \
+  --name=webtop \
+  --security-opt seccomp=unconfined \
+  -e PUID=0 \
+  -e PGID=0 \
+  -e TZ=Etc/UTC \
+  -e SUBFOLDER=/  \
+  -e TITLE=Webtop  \
+  -e DRINODE=/dev/dri/renderD128 \
+  -p 3000:3000 \
+  -p 3001:3001 \
+  -v /data:/config \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --shm-size="21gb"  \
+  --privileged \
+  --gpus all \
+  --runtime nvidia \
+  --device /dev/dri:/dev/dri \
+  --restart unless-stopped \
+  linuxserver/webtop:ubuntu-xfce
